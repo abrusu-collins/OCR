22da1200f7415282444f5cbe2956d66780ce2a1f
abrusu-collins <snillochemingson@gmail.com>
2022-07-02 16:04:21 +0000

    Code formatted

--- app.js
+++ app.js
@@ -3,115 +3,59 @@ const express = require("express");
 let app = express();
 const fs = require("fs");
 const multer = require("multer");
-const  Tesseract = require("tesseract.js");
-// const worker = TesseractWorker();
-app.use(express.static(__dirname + '/public'));
+const Tesseract = require("tesseract.js");
+
+// Using the public folder
+app.use(express.static(__dirname + "/public"));
 
 // setting storage object
 const storage = multer.diskStorage({
-    destination: (req, file, cb)=>{
-     cb(null, "./uploads")
-    },
-    filename:(req,file,cb)=>{
-        cb(null,file.originalname)
-    }
+  destination: (req, file, cb) => {
+    cb(null, "./uploads");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
 });
-const upload =multer ({storage:storage}).single("myImage");
+const upload = multer({ storage: storage }).single("myImage");
 
-let api= {}
+// empty object
+
+let api = {};
 
 // ejs
-app.set("view-engine","ejs");
+app.set("view-engine", "ejs");
 
 //Routes
-
-app.get("/",(req,res)=>{
-
-    res.render("documentreader.ejs")
+//Home route
+app.get("/", (req, res) => {
+  res.render("documentreader.ejs");
 });
-app.post("/uploads",(req, res)=>{
-    upload(req, res, err =>{
-        fs.readFile(`./uploads/${req.file.originalname}`,(err,data)=>{
-            if (err) return console.log("Error");
-            Tesseract.recognize(
-                data,
-                'eng',
-                { logger: m => console.log(m) }
-              ).then(({ data: { text } }) => {
-                api.text= text;
-                console.log(text);
-              })
-        })
+
+// Make a post request, and process image
+app.post("/uploads", (req, res) => {
+  upload(req, res, (err) => {
+    fs.readFile(`./uploads/${req.file.originalname}`, (err, data) => {
+      if (err) return console.log("Error");
+      Tesseract.recognize(data, "eng", { logger: (m) => console.log(m) }).then(
+        ({ data: { text } }) => {
+          api.text = text;
+          console.log(text);
+        }
+      );
     });
-})
+  });
+});
 
-
-app.get("/uploads",(req,res)=>{
-
-    res.send(api);
-
-
+// API roue for generted text
+app.get("/uploads", (req, res) => {
+  res.send(api);
 });
 
 // Port to listen on
-const PORT= 5000||process.env.PORT;
-app.listen(PORT, ()=>{
-    console.log(`Listening on port : ${PORT}`)
+const PORT = 5000 || process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Listening on port : ${PORT}`);
 });
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// app.get("/",(req,res)=>{
-// res.write("Abrusu is doing express");
-// });
-// app.listen(5000);
