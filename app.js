9d66b5cf3d99f0817733ccba5d3cfa2538384a62
abrusu-collins <snillochemingson@gmail.com>
2022-07-05 14:55:05 +0000

    .doc, .docx reader working

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ const fs = require("fs");
 const multer = require("multer");
 const Tesseract = require("tesseract.js");
 const pdf = require("pdf-parse");
+var mammoth = require("mammoth");
 
 app.use(express.static(__dirname + "/public"));
 app.set("view-engine", "ejs");
 
-// setting storage object for image
+// setting storage object for image, doc and pdf
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "./uploads");
@@ -17,16 +18,14 @@ const storage = multer.diskStorage({
     cb(null, file.originalname);
   },
 });
-
-// empty object to store text taken off image
-let api = {};
-
-//setting storage for pdf uploads
-const pdfstorage = multer({ dest: "pdfuploads/" });
 const upload = multer({ storage: storage }).single("myImage");
+const pdfstorage = multer({ dest: "pdfuploads/" });
+const docstorage = multer({ dest: "docuploads/" });
 
-//empty object to store text taken off pdf
-let pdfobject ={}
+// empty object to store text taken off image, pdf and doc
+let api = {};
+let pdfobject = {};
+let docobject = {};
 
 //Routes
 app.get("/", (req, res) => {
@@ -45,9 +44,17 @@ app.get("/uploads", (req, res) => {
   res.send(api);
 });
 
-app.get("/pdfconvert", (req,res)=>{
+app.get("/pdfconvert", (req, res) => {
   res.send(pdfobject);
-})
+});
+
+app.get("/docconvert", (req, res) => {
+  res.send(docobject);
+});
+
+app.get("/docreader", (req, res) => {
+  res.render("docreader.ejs");
+});
 
 app.post("/uploads", (req, res) => {
   upload(req, res, (err) => {
@@ -76,14 +83,21 @@ app.post("/pdfconvert", pdfstorage.single("pdf"), function (req, res) {
   let dataBuffer = fs.readFileSync(`./pdfuploads/${req.file.filename}`);
   pdf(dataBuffer).then(function (data) {
     console.log(data.text);
-    pdfobject.text=data.text;
+    pdfobject.text = data.text;
   });
+});
+
+app.post("/docconvert", docstorage.single("doc"), function (req, res) {
+  mammoth.extractRawText({ path: `./docuploads/${req.file.filename}` })
+    .then(function (result) {
+      docobject.text = result.value;
+    })
+    .done();
 });
 
 app.get("*", (req, res) => {
   res.render("404.ejs");
 });
-
 
 const PORT = 5000 || process.env.PORT;
 app.listen(PORT, () => {
---
+++ public/docreader.js
@@ -0,0 +1,108 @@
+
+let input = document.getElementById("textinput");
+let select = document.getElementById("select");
+let button = document.getElementById("btn");
+let speedgauge = document.getElementById("speed");
+let pitchgauge = document.getElementById("pitch");
+let speedval = document.getElementById("speedval");
+let pitchval = document.getElementById("pitchval");
+let play = document.getElementById("play");
+let upload = document.getElementById("upload");
+let form = document.getElementById("form");
+
+
+pitchgauge.addEventListener("input", pitchincrease, true);
+speedgauge.addEventListener("input", speedincrease, true);
+
+//uploaded successfully
+
+upload.addEventListener("click", (e)=>{
+  
+  if(!(input.value)){
+    alert("Choose a PDF file")
+  }
+  else{
+    alert("File uploaded successfully")
+  }
+})
+
+function speedincrease(e) {
+  speedval.textContent = speedgauge.value;
+}
+
+function pitchincrease(e) {
+  pitchval.textContent = pitchgauge.value;
+}
+
+//loading all voices
+var synth = window.speechSynthesis;
+function allvoices() {
+  window.voices = synth.getVoices();
+  for (var i = 0; i <= voices.length - 1; i++) {
+    let option = document.createElement("option");
+    option.textContent = `${voices[i].name} (${voices[i].lang})`;
+    option.setAttribute("data-lang", voices[i].lang);
+    option.setAttribute("data-name", voices[i].name);
+
+    select.appendChild(option);
+  }
+}
+
+if (synth.onvoiceschanged !== undefined) {
+  synth.onvoiceschanged = allvoices;
+}
+
+//speak 
+function speak(e) {
+  e.preventDefault();
+  
+  fetch("http://localhost:5000/docconvert").then(
+    (res)=>{
+      return res.json()
+    }
+  ).then(
+    (data)=>{
+  
+  var newwindow= window.open('', '', 'width=500, height=500');
+  newwindow.document.write(`<pre>${data.text}</pre>`);
+
+  var utterThis = new SpeechSynthesisUtterance(data.text);
+  var selectedOption = select.selectedOptions[0].getAttribute("data-name");
+  for (var i = 0; i < voices.length; i++) {
+    if (voices[i].name === selectedOption) {
+      utterThis.voice = voices[i];
+    }
+  }
+
+  utterThis.pitch = pitchgauge.value;
+  utterThis.rate = speedgauge.value;
+  synth.speak(utterThis);
+
+  input.blur();
+    }
+  )
+
+  
+}
+
+button.addEventListener("click", speak, true);
+
+//pause,resume and stop
+
+let a=document.getElementById("a")
+let b=document.getElementById("b")
+let c=document.getElementById("c")
+
+  a.addEventListener("click",(e)=>{
+    synth.pause();
+  })
+
+  b.addEventListener("click",(e)=>{
+    synth.resume();
+  })
+
+
+ c.addEventListener("click",(e)=>{
+    synth.cancel();
+    
+  })
