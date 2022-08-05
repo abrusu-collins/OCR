894741e0e544d352ae61179e96992f9be543daf5
abrusu-collins <snillochemingson@gmail.com>
2022-08-05 15:31:36 +0000

    Debugging

--- public/ocr.js
+++ public/ocr.js
@@ -38,7 +38,7 @@ let dat;
 
 //function for API call
 let ApiCall = function () {
-  fetch("http://localhost:5000/uploads",{mode:"cors"}
+  fetch("https://clisteocr.herokuapp.com/uploads",{mode:"cors"}
   )
     .then((response) => {
       return response.json();
